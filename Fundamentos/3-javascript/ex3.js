2c6b3719114fc50f3cbd3b14f00a88069c6ee9a9
Leddro <leandrosantanamartins@hotmail.com>
2026-04-17 11:43:30 -0300

    r ex3js

---
+++ Fundamentos/3-javascript/ex3.js
@@ -0,0 +1,14 @@
+// Utilize if/else para escrever um código que retorne o maior de três números. Defina, no começo do seu código,
+// três variáveis com os valores que serão comparados.
+
+const num1 = 9
+const num2 = 9
+const num3 = 8
+
+if(num1>=num2 && num1>=num3){
+    console.log(num1)
+}
+else if(num2>=num1 && num2>=num3){
+    console.log(num2)
+}
+else(console.log(num3))
